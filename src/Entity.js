01f4320fec6b83fb6e37c732f227ac9bef18ff02
Timo Ebel <timo.ebel@gmail.com>
2019-01-20 18:43:40 +0100

    Removed event handling from entities.

--- src/Entity.js
+++ src/Entity.js
@@ -1,19 +1,15 @@
 /**
  * @implements CommandHandler
- * @implements EventHandler
  */
 class Entity {
   /**
    * @param {Logger} logger
    * @param {CommandDispatcher} commandDispatcher
-   * @param {EventDispatcher} eventDispatcher
    */
-  constructor (logger, commandDispatcher, eventDispatcher) {
+  constructor (logger, commandDispatcher) {
     this.logger = logger
     this.commandDispatcher = commandDispatcher
-    this.eventDispatcher = eventDispatcher
     this.commandHandlerFunctions = {}
-    this.eventHandlerFunctions = {}
   }
 
   /**
@@ -39,26 +35,6 @@ class Entity {
   }
 
   /**
-   * @param {string} name
-   * @param {Function} func
-   */
-  registerEvent (name, func) {
-    if (this.eventHandlerFunctions[name]) {
-      this.logger.error(new Error(`Two functions registered as event handler for ${name}. Keeping the former.`))
-      return
-    }
-
-    this.eventHandlerFunctions[name] = func
-    this.eventDispatcher.subscribe(name, this)
-
-    this.logger.info({
-      name,
-      aggregateClass: this.constructor.name,
-      functionName: func.name
-    }, 'Registered handler function for an event.')
-  }
-
-  /**
    * @param {Command} command
    * @returns {Event[]}
    */
@@ -78,41 +54,6 @@ class Entity {
       'Going to handle a command.'
     )
     return this.commandHandlerFunctions[command.name](command)
-  }
-
-  /**
-   * @param {Event} event
-   * @returns {Event[]}
-   */
-  apply (event) {
-    if (!this.eventHandlerFunctions[event.name]) {
-      this.logger.error(new Error(`Cannot apply incoming event ${event.name || 'no name given'}.`))
-      return []
-    }
-
-    this.logger.debug(
-      {
-        name: event.name,
-        time: event.time,
-        payload: JSON.stringify(event.payload, null, 2),
-        aggregateClass: this.constructor.prototype
-      },
-      'Going to apply an event.'
-    )
-    return this.eventHandlerFunctions[event.name](event)
-  }
-
-  /**
-   * @param {string} name
-   * @param {Object} payload
-   * @returns {Event}
-   */
-  createEvent (name, payload = {}) {
-    return {
-      name,
-      time: new Date().toISOString(),
-      payload
-    }
   }
 }
 
