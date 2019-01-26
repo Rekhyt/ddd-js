142f2a3ee56ff416ad9ee324e87622bf3056397f
Timo Ebel <timo.ebel@gmail.com>
2019-01-26 16:04:18 +0100

    Fixed / adapted unit tests.

--- test/unit/EventDispatcherLocal.test.js
+++ test/unit/EventDispatcherLocal.test.js
@@ -5,6 +5,7 @@ const EventDispatcherLocal = require('../../src/EventDispatcherLocal')
 describe('EventDispatcherLocal', () => {
   let subjectUnderTest
   let logger
+  let eventRepository
 
   beforeEach(() => {
     logger = {
@@ -15,11 +16,15 @@ describe('EventDispatcherLocal', () => {
       error: (...args) => console.log(args)
     }
 
-    subjectUnderTest = new EventDispatcherLocal(logger)
+    eventRepository = {
+      save: (...args) => {}
+    }
+
+    subjectUnderTest = new EventDispatcherLocal(logger, eventRepository)
   })
 
-  describe('dispatch', () => {
-    it('should dispatch a given event to all subscribed handlers', async () => {
+  describe('publish', () => {
+    it('should publish a given event to all subscribed handlers', async () => {
       let handler1CallCounter = 0
       let handler2CallCounter = 0
 
@@ -33,37 +38,10 @@ describe('EventDispatcherLocal', () => {
 
       subjectUnderTest.subscribe('event1', handler1)
       subjectUnderTest.subscribe('event1', handler2)
-      await subjectUnderTest.dispatch({ name: 'event1' })
+      await subjectUnderTest.publish({ name: 'event1' })
 
       assert.strictEqual(handler1CallCounter, 1)
       assert.strictEqual(handler2CallCounter, 1)
-    })
-
-    it('should dispatch events returned by events to all subscribed handlers', async () => {
-      let handler1CallCounter = 0
-      let handler2CallCounter = 0
-      let handler3CallCounter = 0
-
-      const handler1 = {
-        apply: async () => { handler1CallCounter++; return [{ name: 'event2' }, { name: 'event3' }] }
-      }
-
-      const handler2 = {
-        apply: async () => { handler2CallCounter++; return [{ name: 'event3' }] }
-      }
-
-      const handler3 = {
-        apply: async () => { handler3CallCounter++; return [] }
-      }
-
-      subjectUnderTest.subscribe('event1', handler1)
-      subjectUnderTest.subscribe('event2', handler2)
-      subjectUnderTest.subscribe('event3', handler3)
-      await subjectUnderTest.dispatch({ name: 'event1' })
-
-      assert.strictEqual(handler1CallCounter, 1)
-      assert.strictEqual(handler2CallCounter, 1)
-      assert.strictEqual(handler3CallCounter, 2)
     })
 
     it('should log an error if no handler is subscribed for an incoming event', async () => {
@@ -71,7 +49,7 @@ describe('EventDispatcherLocal', () => {
 
       logger.error = () => { loggerCallCount++ }
 
-      await subjectUnderTest.dispatch({ name: 'event' })
+      await subjectUnderTest.publish({ name: 'event' })
 
       assert.strictEqual(loggerCallCount, 1)
     })
