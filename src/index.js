5fb87bf6dd33d0c490cef54aaf46bb576ea83e09
Timo Ebel <timo.ebel@gmail.com>
2020-01-01 15:57:48 +0100

    Added a base entity that other entities can base on to support basic entity versioning.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
+// glue code
 const Runner = require('./Runner')
+
+// aggregate base classes
+const BaseEntity = require('./BaseEntity')
 const RootEntity = require('./RootEntity')
 const ReadModel = require('./ReadModel')
 const Saga = require('./Saga')
+
+// command & event handling
 const CommandDispatcherLocal = require('./CommandDispatcherLocal')
 const EventDispatcherLocal = require('./EventDispatcherLocal')
 const EventDispatcherEventEmitter = require('./EventDispatcherEventEmitter')
+
+// storage
 const EventStoreJsonFile = require('./EventStoreJsonFile')
+
+// errors
 const InvalidTypeError = require('./GenericErrors/InvalidTypeError')
 const InvalidArgumentError = require('./GenericErrors/InvalidArgumentError')
 const OutdatedEntityError = require('./GenericErrors/OutdatedEntityError')
 const SagaError = require('./GenericErrors/SagaError')
 const ValidationError = require('./GenericErrors/ValidationError')
+
+// value objects
 const DateTime = require('./ValueObject/DateTime')
 const EmailAddress = require('./ValueObject/EmailAddress')
 const IntegerVersion = require('./ValueObject/IntegerVersion')
@@ -20,6 +32,7 @@ const Enum = require('./ValueObject/Enum')
 module.exports = {
   Runner,
 
+  BaseEntity,
   RootEntity,
   ReadModel,
   Saga,
